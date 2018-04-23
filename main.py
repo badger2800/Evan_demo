69d439d5b855a69b631910d9638c2af199ccf9ad
nick <nickfielding@hotmail.com>
2018-04-23 15:34:29 +0100

    Updated.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sel_code
-
+import os
+import sys
 
 sel_code.browser_setup()
 sel_code.login()
@@ -7,8 +8,20 @@ sel_code.login()
 # TODO Change parameter, depending on the file you are pulling in.
 sel_code.category_choice("Productos")
 
-# TODO Build file loop here
-file_location = "/home/nick/Downloads/first_1000_products_fixed.csv"
-sel_code.import_files(file_location)
+# Open a folder
+path = "/home/nick/Downloads"
+dirs = os.listdir(path)
+
+# This would print all the files and directories
+for file in dirs:
+    file_location = (path + "/" + file)
+    sel_code.import_files(file_location)
 
 sel_code.browser_close()
+
+
+
+
+
+
+
